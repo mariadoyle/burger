d06f8cb7ef4cd3796d1204216283263e276b32a0
Maria Doyle <mardoyle@hotmail.com>
2016-10-28 00:11:19 -0400

    fixedmysqlproblems

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Vegan Burger', FALSE, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Chicken Burger', FALSE, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Turkey Burger', FALSE, now());
---
+++ models/burger.js
@@ -0,0 +1,26 @@
+var orm = require('../config/orm.js');
+
+var burger = 
+{
+	selectAll: function(cb) 
+	{
+		orm.selectAll('burgers', function(data)
+		{
+			cb(data);
+		})
+	},
+	insertOne: function(col, burger_name, cb)
+	{
+		orm.insertOne('burgers', col, burger_name, function(data)
+		{
+			cb(data);
+		})
+	},
+	updateOne: function(col, burger_id, cb){
+		orm.updateOne('burgers', col, burger_id, function(data)
+		{
+			cb(data);
+		})
+	}
+}
+module.exports = burger;
